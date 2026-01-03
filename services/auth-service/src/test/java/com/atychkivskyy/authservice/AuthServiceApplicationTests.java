73301bfb019de687a37c1cf2792950475be3416f
Andriy Tychkivskyy <atychkivskyy@gmail.com>
2026-01-04 00:40:30 +0100

    test(auth): remove default Spring context test

--- services/auth-service/src/test/java/com/atychkivskyy/authservice/AuthServiceApplicationTests.java
+++ services/auth-service/src/test/java/com/atychkivskyy/authservice/AuthServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.atychkivskyy.authservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Disabled until test infrastructure is in place")
 class AuthServiceApplicationTests {
 
     @Test
